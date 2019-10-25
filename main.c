8313c78e0ea6f7bb70b6522596e58510f79c4a05
deladia <vlbattalov@gmail.com>
2019-10-25 23:02:02 +0300

    add reflection

--- main.c
+++ main.c
@@ -87,33 +87,59 @@ int 		color_scale(int	color, double c)
 	return ((red << 16) | (green << 8) | blue);
 }
 
+t_pos		reflect_ray(t_pos *r, t_pos *n)
+{
+	t_pos	ret;
+	t_pos	a;
+	t_pos	b;
 
+	a = vector_on_number(n, 2);
+	b = vector_on_number(&a, dot(r, n));
+	ret = vector_minus(&b, r);
+	return (ret);
+}
 
 /*
 ** 		Находит ближайшую точку в позиции (x, y) среди всех сфер и возвращает цвет пикселя
 */
 
-int			trace_ray(t_pos *o, t_pos *d, double t_min, double t_max, t_sphere *sphere, t_light *light)
+int			trace_ray(t_pos *o, t_pos *d, double t_min, double t_max, t_sphere *sphere, t_light *light, int depth)
 {
 	t_pos		p;
 	t_pos		n;
 	t_pos		buf;
+	t_pos		r;
 	t_return	ret;
 	double 		c;
+	int 		local_color;
+	int 		reflected_color;
+	double		ref;
 
 	ret = closest_intersection(o, d, t_min, t_max, sphere);
 	if (ret.closest_sphere == NULL)
 		return (BLACK);
 
 	//Часть для добавления бликов от света
+	// 		Вычисление локального цвета
 	buf = vector_on_number(d, ret.closest_t);
 	p = vector_pus(o, &buf);
 	n = vector_minus(&p, ret.closest_sphere->center);
 	n = vector_div(&n, vector_len(&n));
+	//Теперь buf это -D
 	buf = vector_on_number(d, -1);
 	// Вычисление коэфа для умножения на цвет сферы
 	c = computer_lighting(&p, &n, &buf, ret.closest_sphere->specular, light, sphere);
-	return (color_scale(ret.closest_sphere->color, c));
+	local_color = color_scale(ret.closest_sphere->color, c);
+
+	ref = ret.closest_sphere->reflective;
+	if (depth <= 0 || ref <= 0)
+		return (local_color);
+
+	r = reflect_ray(&buf, &n);
+	reflected_color = trace_ray(&p, &r, 0.001, INFINITY, sphere, light, depth - 1);
+
+	return (color_scale(local_color, 1 - ref) + color_scale(reflected_color, ref));
+
 }
 
 
@@ -181,7 +207,6 @@ int			main(void)
 	t_sdl		*sdl;
 	t_light		*light;
 
-
 	sdl = (t_sdl *)ft_memalloc((sizeof(t_sdl)));
 	sdl_init(sdl);
 
@@ -204,7 +229,7 @@ int			main(void)
 		{
 			d->position = canvas_to_viewport(x, y, d->position);
 //			printf("%f %f %f\n", d->position->x, d->position->y, d->position->z);
-			color = trace_ray(o->position, d->position, 1.0, INFINITY, sphere, light);
+			color = trace_ray(o->position, d->position, 1.0, INFINITY, sphere, light, 3);
 			sdl->pixels = put_pixel(x, y, color, sdl);
 			y++;
 		}
