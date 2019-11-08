d6780a5130aa7da04eebe1c638312b9ed1e36f2d
Maksim <prokushevmaksim@gmail.com>
2019-11-08 17:05:26 +0300

    norminette scene

--- src/main.c
+++ src/main.c
@@ -48,7 +48,7 @@ int		validate(t_scene *scene, int ac, char **av)
 		return (0);
 	}
 	if (ft_strcmp(av[1], "map_one") == 0)
-		init_scene(scene);
+		map_one(scene);
 	else
 	{
 		ft_putstr("input \"map_one\" or \"map_one\" ");
