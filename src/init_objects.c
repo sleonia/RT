c8eca199fd5b814a4485a26edc929c1b976fe1bc
dolovnyak <dolovnyak@yandex.ru>
2019-11-08 12:50:21 +0300

    del trash

--- src/init_objects.c
+++ src/init_objects.c
@@ -1,6 +1,5 @@
 #include "rtv1.h"
 
-
 t_object	*init_cone(t_object *obj, t_cone_params params)
 {
 	if (obj == NULL)
@@ -154,6 +153,5 @@ t_object 	*init_scene(void)
 	object = init_sphere(object,  (t_sphere_params){{-2.0, 2.0, 3.0}, 1.0, 0xFF00FF, 500, 0.2});
 	object = init_cone(object, (t_cone_params){{-2.0, 2.0, 3.0}, 0.2, 0xFFFFFF, 500, 0.2, {1.0, 0.0, 0.0}});
 	object = init_plane(object,  (t_plane_params){{-2.0, 0.0, 3.0}, 0xFFFFFF, 500, 0.2, {0.0, 0.0, -1.0}});
-//	object
 	return (object);
 }
