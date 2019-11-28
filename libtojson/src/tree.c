1d28c13470c4fa2a76510bdeba27094bebdc04de
Tsun Horker <DCCXXVI726726@gmail.com>
2019-11-28 21:47:02 +0300

    some norm

--- libtojson/src/tree.c
+++ libtojson/src/tree.c
@@ -6,24 +6,48 @@
 /*   By: deladia <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/03 20:55:14 by delalia           #+#    #+#             */
-/*   Updated: 2019/11/26 19:28:12 by thorker          ###   ########.fr       */
+/*   Updated: 2019/11/28 21:31:31 by thorker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "to_json.h"
-/*
- *	Функция которая проверяет имя и вызывает функцию для обработки значения
- */
-int				check_value_and_name(t_key_value *for_re, t_token **token)
+
+char				*create_string(char *str)
 {
 	char	*new_key;
-	void	*new_value;
-	t_type	new_type;
+	size_t	k;
+	size_t	i;
 
-	new_type = 0;
+	k = 0;
+	i = 0;
+	while (*(str + k) != 0)
+	{
+		if (*(str + k) != '\\')
+			i++;
+		else if (*(str + ++k) == 0)
+			return (0);
+		k++;
+	}
+	if ((new_key = ft_strnew(i)) == 0)
+		return (0);
+	i = 0;
+	k = 0;
+	while (*(str + k) != 0)
+	{
+		if (*(str + k) == '\\')
+			k++;
+		*(new_key + i++) = *(str + k++);
+	}
+	return (new_key);
+}
+
+char				*check_key(t_token **token)
+{
+	char	*new_key;
+
 	if ((*token = (*token)->next) == 0)
 		return (0);
-	if ((new_key = ft_strdup((*token)->value)) == 0)
+	if ((new_key = create_string((*token)->value)) == 0)
 		return (0);
 	if ((*token = (*token)->next) == 0 ||
 			ft_strcmp((*token)->value, "\"") != 0 ||
@@ -34,7 +58,22 @@ int				check_value_and_name(t_key_value *for_re, t_token **token)
 		ft_strdel(&new_key);
 		return (0);
 	}
-	
+	return (new_key);
+}
+
+/*
+**	Функция которая проверяет имя и вызывает функцию для обработки значения
+*/
+
+int					check_value_and_name(t_key_value *for_re, t_token **token)
+{
+	char	*new_key;
+	void	*new_value;
+	t_type	new_type;
+
+	new_type = 0;
+	if ((new_key = check_key(token)) == 0)
+		return (0);
 	if (ft_str_isdigit((*token)->value) != 0)
 	{
 		if ((new_value = check_digit(token)) != *token)
@@ -47,54 +86,68 @@ int				check_value_and_name(t_key_value *for_re, t_token **token)
 	}
 	else if (realloc_key_value(for_re, new_key, new_value, new_type) == 0)
 	{
-			ft_strdel(&new_key);
-			free(new_value);
-			return (0);
+		ft_strdel(&new_key);
+		free(new_value);
+		return (0);
 	}
 	return (1);
 }
 
 /*
- *	функция, которая проверяет валидность объектов
- */
+** функция в которой создается пустая структура
+*/
 
-void		*check_object(t_token **token)
+static t_key_value	*create_empty_struct(void)
+{
+	t_key_value	*for_re;
+
+	if ((for_re = (t_key_value*)malloc(sizeof(t_key_value))) == 0)
+		return (0);
+	if ((for_re->key = (char**)malloc(sizeof(char*))) == 0)
+	{
+		free(for_re);
+		return (0);
+	}
+	if ((for_re->value = (void**)malloc(sizeof(void*))) == 0)
+	{
+		free(for_re->key);
+		free(for_re);
+		return (0);
+	}
+	*(for_re->key) = 0;
+	*(for_re->value) = 0;
+	for_re->type = 0;
+	return (for_re);
+}
+
+/*
+**	функция, которая проверяет валидность объектов
+*/
+
+void				*check_object(t_token **token)
 {
 	t_key_value		*for_re;
 
-	//разделить проверки
-	if ((*token)->next != 0 && ft_strcmp(((*token)->next)->value, "}") == 0)
+	if ((*token = (*token)->next) != 0 && ft_strcmp((*token)->value, "}") == 0)
 		return (0);
-	else
-	{
-		for_re = (t_key_value*)malloc(sizeof(t_key_value));
-		for_re->key = (char**)malloc(sizeof(char*));
-		*(for_re->key) = 0;
-		for_re->value = (void**)malloc(sizeof(void*));
-		*(for_re->value) = 0;
-		for_re->type = 0;
-	}
-	*token = (*token)->next;
+	else if ((for_re = create_empty_struct()) == 0)
+		return (*token);
 	while (*token != 0)
 	{
 		if (ft_strcmp((*token)->value, "\"") == 0)
 		{
 			if (check_value_and_name(for_re, token) == 0)
+				break ;
+			if (ft_strcmp((*token)->value, ",") == 0)
 			{
-				ft_return(&for_re);
-				return (*token);
+				*token = (*token)->next;
+				continue;
 			}
 		}
-		if (ft_strcmp((*token)->value, ",") == 0)
-			*token = (*token)->next;
-		else if (ft_strcmp((*token)->value, "}") == 0)
+		if (ft_strcmp((*token)->value, "}") == 0)
 			return (for_re);
-		else
-		{
-			ft_return(&for_re);
-			return (*token);
-		}
+		break ;
 	}
+	ft_return(&for_re);
 	return (*token);
 }
-//что делать если   token == 0
