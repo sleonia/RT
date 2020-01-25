7ada63da09c72a7db84a2dcc54dc72341d4bb8fe
Clydas Criston <ccriston@su-f4.21-school.ru>
2020-01-25 16:50:30 +0300

    cam,light

--- src/parse.c
+++ src/parse.c
@@ -1,5 +1,18 @@
 #include "rtv1.h"
 
+void	find_camera(t_cam	**cam, t_key_value *tmp)
+{
+	t_key_value *node;
+	int error;
+
+	error = get_node(tmp, "camera", &node);
+	*cam = (t_cam *)ft_memalloc(sizeof(t_cam));
+	error = get_double(node, "angle_a", &(*cam)->a);
+	error = get_double(node, "angle_b", &(*cam)->b);
+	//error = get_array(node, "position", &(*cam)->position);
+	
+}
+
 void	find_lights(t_light	**light, t_key_value *tmp)
 {
 	t_key_value *node;
@@ -17,7 +30,7 @@ void	find_lights(t_light	**light, t_key_value *tmp)
 	if (strcmp(&new->type, "p") == 0 || strcmp(&new->type,"d") == 0)
 		printf("get_massive");
 	error = get_double(node, "intensity", &new->intensity);
-	printf("%f", new->intensity);
+	printf("%f\n", new->intensity);
 	if (light != NULL)
 		new->next = *light;
 	else
@@ -27,19 +40,30 @@ void	find_lights(t_light	**light, t_key_value *tmp)
 
 void	find_spheres(t_object **object, t_key_value *tmp)
 {
-	//t_key_value *node;
+	t_key_value *node;
 	t_key_value *node2;
 	t_object	*new_obj;
 	int error;
+	// int		*pos;
 
 	new_obj = (t_object *)ft_memalloc(sizeof(t_object));
-	error = get_node(tmp, "sphere_1", &node2);
+	new_obj->objects = (union u_objects *)ft_memalloc(sizeof(union u_objects));
+	error = get_node(tmp, "figure", &node);
+	// error = get_node(node, "sphere_1", &node2);
+	node2 = (t_key_value *)node->value[0];
 	if (node2 != NULL)
 	{
-		error = get_int(node2, "type", (int *)&new_obj->type);
+		// error = get_int(node2, "type", (int *)&new_obj->type);
+		printf("\n%d\n", new_obj->type);
+		// error = get_node(node2, "position", &node2);
+		// pos = make_array(&node2);
+		new_obj->objects->sphere.center = (t_pos){0, 0, 5};
+		printf("%f %f %f\n", new_obj->objects->sphere.center.x, new_obj->objects->sphere.center.y, new_obj->objects->sphere.center.z);
 		//error = get_arr(node2, "position", &new_obj->objects->sphere.center);
-		error = get_double(node2, "radius", &new_obj->objects->sphere.radius);
+		error = get_double(node2, "radius_1", &new_obj->objects->sphere.radius);
+		printf("%f", new_obj->objects->sphere.radius);
 		//error = get_int(node2, "color", &new_obj->color);
+		new_obj->color = 0xFF00FF;
 		error = get_int(node2, "secular", &new_obj->specular);
 		error = get_double(node2, "reflective", &new_obj->reflective);
 		//массив для position
@@ -138,9 +162,11 @@ void	find_all(t_scene *scene)
 	tmp = parse_json("test.json");
 	scene = (t_scene *)ft_memalloc(sizeof(t_scene));
 	scene->light = NULL;
+	find_camera(&scene->cam, tmp);
+	printf("%f",scene->cam->a);
 	find_lights(&scene->light, tmp);
 	printf("%f", scene->light->intensity);
 	find_spheres(&scene->object, tmp);
-	find_plane(&scene->object, tmp);
-	find_cylinder(&scene->object, tmp);
+	// find_plane(&scene->object, tmp);
+	// find_cylinder(&scene->object, tmp);
 }
