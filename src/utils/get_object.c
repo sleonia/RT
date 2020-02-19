c04098e14b10f8ba4c0f11e3aa7060d9986dacce
Clydas Criston <ccriston@oa-j5.21-school.ru>
2020-02-19 22:28:56 +0300

    fixed get_object and norms

--- src/utils/get_object.c
+++ src/utils/get_object.c
@@ -3,28 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   get_object.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sleonia <sleonia@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ccriston <ccriston@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 18:38:23 by deladia           #+#    #+#             */
-/*   Updated: 2020/02/19 21:58:24 by sleonia          ###   ########.fr       */
+/*   Updated: 2020/02/19 22:27:48 by ccriston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-t_object		*get_object(t_scene *s, int x, int y)
+static t_object		*check(t_scene *s, cl_float2 dist, cl_float3 d, int i)
 {
-	cl_float3	d;
 	t_object	*closest;
-	cl_float2	dist;
-	int			i;
 
-	d = cl_minus(cl_mult_n(s->cam.ox, ((float)x * 1.f / WIDTH) - 0.5f),
-		cl_mult_n(s->cam.oy, ((float)y * 1.f / WIDTH) - 0.5f));
-	d = cl_minus(d, s->cam.oz);
-	cl_normalize(&d);
-	dist.x = MAX_DIST + 1.f;
-	i = -1;
 	while (++i < s->count_objects)
 	{
 		if (s->object[i].type == o_sphere && type_sphere(s->cam.pos, d,
@@ -47,4 +38,20 @@ t_object		*get_object(t_scene *s, int x, int y)
 			closest = &s->object[i];
 	}
 	return ((dist.x < MAX_DIST) ? closest : NULL);
+}
+
+t_object		*get_object(t_scene *s, int x, int y)
+{
+	cl_float3	d;
+	t_object	*closest;
+	cl_float2	dist;
+	int			i;
+
+	d = cl_minus(cl_mult_n(s->cam.ox, ((float)x * 1.f / WIDTH) - 0.5f),
+		cl_mult_n(s->cam.oy, ((float)y * 1.f / WIDTH) - 0.5f));
+	d = cl_minus(d, s->cam.oz);
+	cl_normalize(&d);
+	dist.x = MAX_DIST + 1.f;
+	i = -1;
+	return (check(s, dist, d, i));
 }
